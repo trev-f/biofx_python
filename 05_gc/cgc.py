2f00454f0a74bec403788b5961d1406e7a6b39b2
Trevor Freeman <trvrfreeman@gmail.com>
2023-02-27 18:38:04 +0000

    Refactor compute FASTA gc percents to separate methods

--- 05_gc/cgc.py
+++ 05_gc/cgc.py
@@ -30,20 +30,58 @@ class CGC:
 
 
     def solve(self) -> str:
-        records = SeqIO.parse(self._args.file.name, 'fasta')
+        """Solve the compute GC content problem
 
-
-        gc_percents = {}
-        for record in records:
-            gc_count = 0
-            for base in record.seq:
-                if base in 'GCgc':
-                    gc_count += 1
-            gc_percent = 100 * gc_count / len(record.seq)
-
-            gc_percents[record.id] = gc_percent
+        :return: Sequence ID and GC content
+        :rtype: str
+        """
+        gc_percents = self.compute_fasta_records_percent_gc(fasta=self._args.file)
 
         return gc_percents
+
+
+    def compute_fasta_records_percent_gc(self, fasta: TextIO | str) -> dict[str, float]:
+        """Compute percent GC for every record in a FASTA file
+
+        :param fasta: FASTA file handle or file path
+        :type fasta: TextIO | str
+        :return: Sequence ID key and percent GC value for each sequence in fasta
+        :rtype: dict[str, float]
+        """
+        records = SeqIO.parse(fasta, format='fasta')
+        gc_percents = {record.id: self.compute_percent_gc(dna=record.seq) for record in records}
+
+        return gc_percents
+
+
+    def compute_percent_gc(self, dna: str) -> float:
+        """Compute the percent GC of a DNA sequence
+
+        :param dna: DNA sequence
+        :type dna: str
+        :return: Percent of G, C, g, or c resideues
+        :rtype: float
+        """
+        gc_count = self.count_gc(dna=dna)
+        gc_percent = 100 * gc_count / len(dna)
+
+        return gc_percent
+
+
+    def count_gc(self, dna: str) -> int:
+        """Count the number of G or C residues in a DNA sequence
+
+        :param dna: DNA sequence
+        :type dna: str
+        :return: Count of G, C, g, or c)
+        :rtype: int
+        """
+        gc_count = 0
+        for base in dna:
+            if base in 'GCgc':
+                gc_count += 1
+
+        return gc_count
 
 
 @dataclass(frozen=True)
